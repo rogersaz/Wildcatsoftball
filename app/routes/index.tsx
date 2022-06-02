ba6aa03e52a0843b15445959515c625a789e61b9
Mark <45799393+rogersaz@users.noreply.github.com>
2022-06-02 11:58:00 -0700

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -90,21 +90,7 @@ export default function Index() {
                 alt: "Testing Library",
                 href: "https://testing-library.com",
               },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-                alt: "Prettier",
-                href: "https://prettier.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-                alt: "ESLint",
-                href: "https://eslint.org",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
-                alt: "TypeScript",
-                href: "https://typescriptlang.org",
-              },
+              
             ].map((img) => (
               <a
                 key={img.href}
